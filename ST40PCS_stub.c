9cd0dcdadf5e9e68347267891658a6313cbcdcaf
Peter Milne <peter.milne@d-tacq.com>
2016-07-04 15:48:53 +0100

    fake feedback algorithm. Uses new global defines. Also extends to two boxes. OK, duplicates, it's easier. The "algorithm" doesn't know or care how many boxes are in fact present. 	modified:   ST40PCS_stub.c

--- ST40PCS_stub.c
+++ ST40PCS_stub.c
@@ -19,6 +19,8 @@
 #include "ST40PCS_stub.h"
 #include "sysdef.h"
 
+#include "ST40PCS_if.h"
+
 ExtU_ST40PCS_T ST40PCS_U;
 ExtY_ST40PCS_T ST40PCS_Y;
 
@@ -37,25 +39,42 @@ void ST40PCS_initialize(void)
 
 static int iter;
 
-/** fake "feedback algorithm" */
-void ST40PCS_step(void)
+void _ST40PCS_step(short *AI, short* AO, short* DO)
 {
 	int ic;
-	ST40PCS_Y.DTACQOUT[0] = ST40PCS_U.DTACQIN[0];
-	ST40PCS_Y.DTACQOUT[1] = -ST40PCS_U.DTACQIN[0];
+	AO[0] = AI[0];
+	AO[1] = -AI[0];
 
-	ST40PCS_Y.DTACQOUT[2] = 20*iter;
-	ST40PCS_Y.DTACQOUT[3] = -20*iter;
+	AO[2] = 20*iter;
+	AO[3] = -20*iter;
 
 	for (ic = 4; ic < 32; ++ic){
-		ST40PCS_Y.DTACQOUT[ic] = 10*iter + ic*500;
+		AO[ic] = 10*iter + ic*500;
 	}
 
 	/* 16 channels DO looped back to 16 channels DI */
 
-	ST40PCS_Y.DTACQOUT[32] = iter<<8 | 		//counter
+	DO[0] = iter<<8 | 		//counter
 		(ST40PCS_U.DTACQIN[0]>0? 1<<1: 0) |	//mod(AI0)
 		(iter>>1&1? 1<<0:0);	 		//toggles at SR
+
+	++iter;
+}
+/** fake "feedback algorithm"
+ * Example doubles up to provide SAME function both boxes.
+ * we don't know if both boxes are present (and we don't care)..
+ */
+void ST40PCS_step(void)
+{
+	_ST40PCS_step(
+		ST40PCS_U.DTACQIN  + MSI_PCS1_AI,
+		ST40PCS_Y.DTACQOUT + MSI_PCS1_AO,
+		ST40PCS_Y.DTACQOUT + MSI_PCS1_DO);
+
+	_ST40PCS_step(
+		ST40PCS_U.DTACQIN  + MSI_PCS2_AI,
+		ST40PCS_Y.DTACQOUT + MSI_PCS2_AO,
+		ST40PCS_Y.DTACQOUT + MSI_PCS2_DO);
 
 	++iter;
 }
